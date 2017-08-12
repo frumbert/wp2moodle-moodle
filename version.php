64ab135d3b958d843e693177a4be33759052dd75
Tim St.Clair <tim.stclair@gmail.com>
2017-08-13 08:23:28 +1000

    Updated moodle fields

--- version.php
+++ version.php
@@ -28,3 +28,5 @@ defined('MOODLE_INTERNAL') || die();
 $plugin->version   = 2015081000;        // The current plugin version (Date: YYYYMMDDXX)
 $plugin->requires  = 2011112900;        // Requires this Moodle version
 $plugin->component = 'auth_wp2moodle';       // Full name of the plugin (used for diagnostics)
+$plugin->maturity = MATURITY_STABLE;
+$plugin->release = '1.1';
